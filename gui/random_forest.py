a75500628d155397da6ede7454fccbe55e8c25f9
Bytewaiser <bytewaiser@gmail.com>
2022-12-09 00:45:14 +0300

    Catboost and RandomForest max_depth fixed to 100

--- gui/random_forest.py
+++ gui/random_forest.py
@@ -123,7 +123,7 @@ class RandomForest:
         model_parameters_frame.grid(column=1, row=0, rowspan=3, columnspan=2)
         
         parameter_names = ["N Estimators", "Max Depth", "Min Samples Split", "Min Samples Leaf"]
-        self.parameters = [tk.IntVar(value=100), tk.Variable(value="None"), tk.IntVar(value=2), tk.IntVar(value=1)]
+        self.parameters = [tk.IntVar(value=100), tk.IntVar(value=100), tk.IntVar(value=2), tk.IntVar(value=1)]
         self.optimization_parameters = [[tk.IntVar(value=75), tk.IntVar(value=150)], [tk.IntVar(value=5), tk.IntVar(value=15)], [tk.IntVar(value=2), tk.IntVar(value=4)], [tk.IntVar(value=1), tk.IntVar(value=4)]]
         
         ttk.Label(model_parameters_frame, text="Current").grid(column=1, row=0)
@@ -432,17 +432,6 @@ class RandomForest:
             if self.gs_cross_val_option.get() and self.gs_cross_val_var.get() < 2:
                 raise Exception
 
-            # for i, j in enumerate(["Epsilon", "Nu", "C", "Gamma", "Coef0", "Degree"]):
-            #    if str(self.model_parameters_frame_options[i][1]["state"]) != "disabled" and not self.parameters[i].get():
-            #        msg = "Enter a valid " + j +  " value"
-            #        raise Exception
-                
-            #    if self.grid_option_var.get():
-            #        if str(self.model_parameters_frame_options[i][2]["state"]) != "disabled":
-            #            if (not self.optimization_parameters[i][0].get() or not self.optimization_parameters[i][1].get()):
-            #                msg = "Enter a valid " + j +  " value in grid search area"
-            #                raise Exception
-
         except:
             popupmsg(msg) # type: ignore
             return True
@@ -507,7 +496,6 @@ class RandomForest:
         if self.grid_option_var.get() == 0:
             n_estimators = self.parameters[0].get()
             max_depth = self.parameters[1].get()
-            max_depth = int(max_depth) if max_depth != "None" else None
             min_samples_split = self.parameters[2].get()
             min_samples_leaf = self.parameters[3].get()
 
